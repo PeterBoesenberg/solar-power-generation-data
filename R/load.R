2cc61d867235c0555f5ae3d61775f93fd02dfc71
Peter Bösenberg <peterboesenberg@gmail.com>
2020-09-14 09:40:27 +0200

    load all data in a single functin

--- R/load.R
+++ R/load.R
@@ -1,11 +1,35 @@
 library(data.table)
 
 get_load_module <- function() {
+  load_p1_generation_data <- function() {
+    data <- fread("input/Plant_1_Generation_Data.csv")
+    return(data)
+  }
+  load_p1_weather_sensor_data <- function() {
+    data <- fread("input/Plant_1_Weather_Sensor_Data.csv")
+    return(data)
+  }
+  load_p2_generation_data <- function() {
+    data <- fread("input/Plant_2_Generation_Data.csv")
+    return(data)
+  }
+  load_p2_weather_sensor_data <- function() {
+    data <- fread("input/Plant_2_Weather_Sensor_Data.csv")
+    return(data)
+  }
+  
   env <- new.env()
   
-  #public functions
-  env$load_generation_data <- function() {
-    data <- fread("input/Plant_1_Generation_Data.csv")
+  # public functions
+  
+  # load all data in a list with named attributes
+  # @return list wiith p1_gen, p1_weather, p2_gen, p2_weather
+  env$load <- function() {
+    data <- list()
+    data$p1_gen <- load_p1_generation_data()
+    data$p1_weather <- load_p1_weather_sensor_data()
+    data$p2_gen <- load_p2_generation_data()
+    data$p2_weather <- load_p2_weather_sensor_data()
     return(data)
   }
  
--- app.R
+++ app.R
@@ -3,3 +3,4 @@ library(data.table)
 source("R/load.R")
 
 load <- get_load_module()
+data <- load$load()
